d827a5c90153d6febc8035e22967d0da2dc2f9c1
Carlos Bastidas <carlos.bastidas2911@gmail.com>
2024-04-09 00:42:33 -0400

    se esta configurando el ghpages

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,21 +1,16 @@
-
-
-
 import React, { useEffect } from "react";
 
-import { Route, Routes, useNavigate } from 'react-router-dom'
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 /* @Utils */
 
-
-import { routes } from 'routes'
-import { TRoute } from 'types'
-import { typeRouteEnum } from 'lib/enums'
+import { routes } from "routes";
+import { TRoute } from "types";
+import { typeRouteEnum } from "lib/enums";
 import NAVIGATION from "routes/navigation";
 import { MainLayout, PublicLayout, Page404 } from "components/index";
 
 const App: React.FC = () => {
-
   const clearCache = () => {
     const script = document.querySelector('script[src*="reCAPTCHA"]');
 
@@ -23,70 +18,75 @@ const App: React.FC = () => {
     if (script) {
       document.body.removeChild(script);
     }
-  }
+  };
 
-  const navigate = useNavigate()
-  const accessPublic = [typeRouteEnum.PUBLIC, typeRouteEnum.COMPOSED] as string[]
-  const accessPrivate = [typeRouteEnum.PRIVATE, typeRouteEnum.COMPOSED] as string[]
-
-
-
-
+  const navigate = useNavigate();
+  const accessPublic = [
+    typeRouteEnum.PUBLIC,
+    typeRouteEnum.COMPOSED,
+  ] as string[];
+  const accessPrivate = [
+    typeRouteEnum.PRIVATE,
+    typeRouteEnum.COMPOSED,
+  ] as string[];
 
   /*   /* Verificar token 
     useEffect(() => {
       dispatch(reloadingBrowser());
     }, [dispatch]); */
 
+  useEffect(() => {
+    clearCache();
+  }, []);
 
   useEffect(() => {
-    clearCache()
-  }, [])
-
-  useEffect(() => {
-    if (window.location.pathname === '/') {
-      navigate(NAVIGATION.LOGIN)
+    if (window.location.pathname === "/") {
+      navigate(NAVIGATION.HOME);
     }
-  }, [navigate])
+  }, [navigate]);
   /* 
     useEffect(() => {
       if (!isEmpty(user?.cedula) && isAuthenticated)
         dispatch(userDataWorker({ cedula: user?.cedula }));
     }, [dispatch, user, isAuthenticated]); */
 
-
-
   return (
     <>
-
       <Routes>
         {/* Rutas privadas */}
         <Route element={<MainLayout />}>
           {routes
-            .filter((route: TRoute) => accessPrivate.includes(route?.typeRoute as string))
+            .filter((route: TRoute) =>
+              accessPrivate.includes(route?.typeRoute as string)
+            )
             .map((route: TRoute) => (
-              <Route key={route.path} path={route.path} element={<route.component />} />
+              <Route
+                key={route.path}
+                path={route.path}
+                element={<route.component />}
+              />
             ))}
         </Route>
         {/* Rutas publicas */}
         <Route element={<PublicLayout />}>
           {routes
-            .filter((route: TRoute) => accessPublic.includes(route?.typeRoute as string))
+            .filter((route: TRoute) =>
+              accessPublic.includes(route?.typeRoute as string)
+            )
             .map((route: TRoute) => (
-              <Route key={route.path} path={route.path} element={<route.component />} />
+              <Route
+                key={route.path}
+                path={route.path}
+                element={<route.component />}
+              />
             ))}
         </Route>
         {/* Rutas no encontradas */}
         <Route
-          path='*'
-          element={
-            window.location.pathname !== '/' && (
-              <Page404 />
-            )
-          }
+          path="*"
+          element={window.location.pathname !== "/" && <Page404 />}
         />
       </Routes>
-
     </>
   );
 };
